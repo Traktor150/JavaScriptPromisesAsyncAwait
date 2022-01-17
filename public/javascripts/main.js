727ae95534d533b249ae2de56413e967b19663f7
William <williambecker321@gmail.com>
2022-01-17 12:55:59 +0100

    Changed to use Fetch.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -2,15 +2,31 @@
 
 console.log('main is alive');
 
-const XHR = new XMLHttpRequest();
-XHR.open('GET', 'http://localhost:3000/data');
-XHR.responseType = 'json';
-XHR.send();
+// const XHR = new XMLHttpRequest();
+// XHR.open('GET', 'http://localhost:3000/data');
+// XHR.responseType = 'json';
+// XHR.send();
 
-XHR.onload = () => {
-    const data = XHR.response;
-    console.log('reseved data');
+// XHR.onload = () => {
+//     const data = XHR.response;
+//     console.log('reseved data');
     
+//     const list = document.getElementById('list');
+
+//     data.forEach(element => {
+//         let listObject = document.createElement('li');
+//         listObject.innerText = element;
+//         list.append(listObject);
+//     });
+// };
+
+fetch('http://localhost:3000/data')
+.then(response => response.json())
+.then(data => showData(data));
+
+
+
+const showData = (data) => {
     const list = document.getElementById('list');
 
     data.forEach(element => {
@@ -18,5 +34,4 @@ XHR.onload = () => {
         listObject.innerText = element;
         list.append(listObject);
     });
-
-};
+}
